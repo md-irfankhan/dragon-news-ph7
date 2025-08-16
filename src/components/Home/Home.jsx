aa409da8d985fff3c485df5c1009ea5f899ddb29
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-08-16 08:09:16 +0200

    category button added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 
 
 const Home = () => {
+    const [categories,setCategories]=useState([])
+    useEffect(()=>{
+        fetch('/api/categories.json')
+        .then(res=>res.json())
+        .then(data=>setCategories(data))
+    },[])
     return (
-        <div>
-            Home
-        </div>
+       <main className="grid grid-cols-12 max-w-[1250px] mx-auto">
+        <aside className="col-span-3">
+            <h1 className="font-bold">All Category</h1>
+           <div className="flex flex-col gap-y-2">
+              {
+                categories.map(cate=><button key={cate.id}>{cate.name}</button>)
+              }
+           </div>
+        </aside>
+        <section className="col-span-6">
+        <h1 className="font-bold">Dragon News Home</h1>
+        </section>
+        <aside className="col-span-3">
+        <h1 className="font-bold">Login With</h1>
+        </aside>
+       </main>
     );
 };
 
