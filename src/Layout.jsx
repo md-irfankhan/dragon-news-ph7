9fa582d3f762e0dfd30b50c3ce3558133154caf4
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-08-16 07:54:27 +0200

    Navbar added

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from 'react-router';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 
 const Layout = () => {
     return (
         <div>
             <Header></Header>
+            <nav className='max-w-[1250px] mx-auto'>
+                <Nav></Nav>
+            </nav>
             <Outlet></Outlet>
         </div>
     );
